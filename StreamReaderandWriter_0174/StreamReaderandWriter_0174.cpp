434be2c4a7785879f1e6adcecf1d472156a4c32a
thekill022 <gibfathoni@gmail.com>
2024-06-04 10:13:19 +0700

    unlimited loop untuk menulis file

--- StreamReaderandWriter_0174/StreamReaderandWriter_0174.cpp
+++ StreamReaderandWriter_0174/StreamReaderandWriter_0174.cpp
@@ -13,5 +13,13 @@ int main()
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+
+    }
     
 }
