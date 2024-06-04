adab65e5d9e1a29770e22b62b8387e7e71d492ec
thekill022 <gibfathoni@gmail.com>
2024-06-04 10:36:08 +0700

    membuat input untuk menangkap nama file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -8,5 +8,9 @@ int main()
 
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
     
 }
